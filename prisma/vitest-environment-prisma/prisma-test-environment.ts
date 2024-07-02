652b2615e2f74977f0323d9dc7d664c2bbac2720
Victor Canto <iamvictorcanto@gmail.com>
2024-07-02 01:25:40 -0300

    chore: configure test database per environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,12 +1,36 @@
+import "dotenv/config";
+
+import { prisma } from "@/lib/prisma";
+import { execSync } from "node:child_process";
+
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+
+function generateDatabaseURL(schema: string) {
+    if (!process.env.DATABASE_URL) {
+        throw new Error("Please provide a DATABASE_URL environment variable.");
+    }
+
+    const url = new URL(process.env.DATABASE_URL);
+    url.searchParams.set("schema", schema);
+    return url.toString();
+}
 
 export default <Environment>(<unknown>{
     name: "prisma",
     async setup() {
-        console.log("Setting up prisma test environment");
+        const schema = randomUUID();
+        const databaseURL = generateDatabaseURL(schema);
+        process.env.DATABASE_URL = databaseURL;
+
+        execSync("npx prisma migrate deploy");
+
         return {
             async teardown() {
-                console.log("Tearing down prisma test environment");
+                await prisma.$executeRawUnsafe(
+                    `DROP SCHEMA IF EXISTS "${schema}" CASCADE;`
+                );
+                await prisma.$disconnect();
             },
         };
     },
