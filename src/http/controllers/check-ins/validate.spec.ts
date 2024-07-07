8902822d2985aa05b42562abf733b8a281313af0
Victor Canto <iamvictorcanto@gmail.com>
2024-07-07 00:39:36 -0300

    test: ensure that the user authenticates with their role to access allowed endpoints

--- src/http/controllers/check-ins/validate.spec.ts
+++ src/http/controllers/check-ins/validate.spec.ts
@@ -15,7 +15,7 @@ describe("Validate a Check-in (e2e)", () => {
     });
 
     it("should be able to validate a check-in", async () => {
-        const { token } = await createAndAuthenticateUser(app);
+        const { token } = await createAndAuthenticateUser(app, true);
         const user = await prisma.user.findFirstOrThrow();
         const gym = await prisma.gym.create({
             data: {
